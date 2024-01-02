87c3a9c0a866cf1d617a6d4690b516f392735268
Roman Gerloff <71276897+RomanGoEmpire@users.noreply.github.com>
2024-01-02 17:44:37 +0100

    Refactor bot.py to use integer for ROLE_MESSAGE_ID and update discord.Client instantiation

--- bot.py
+++ bot.py
@@ -11,32 +11,35 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-ROLE_MESSAGE_ID = os.getenv('ROLE_MESSAGE_ID')
-REACTION = os.getenv('REACTION')
-ROLE = os.getenv('ROLE')
+ROLE_MESSAGE_ID = int(os.getenv("ROLE_MESSAGE_ID"))
+REACTION = os.getenv("REACTION")
+ROLE = os.getenv("ROLE")
 
 
-bot = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.default()
+bot = discord.Client(intents=intents)
+
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
-    
+    print(f"{bot.user.name} has connected to Discord!")
+
+
 @bot.event
 async def on_raw_reaction_add(payload):
-    if payload.message_id == ROLE_MESSAGE_ID and payload.emoji.name == REACTION: 
+    if payload.message_id == ROLE_MESSAGE_ID and payload.emoji.name == REACTION:
         guild = bot.get_guild(payload.guild_id)
         role = discord.utils.get(guild.roles, name=ROLE)
         await payload.member.add_roles(role)
-
+        
 @bot.event
 async def on_raw_reaction_remove(payload):
-    if payload.message_id == ROLE_MESSAGE_ID and payload.emoji.name == REACTION: 
+    if payload.message_id == ROLE_MESSAGE_ID and payload.emoji.name == REACTION:
         guild = bot.get_guild(payload.guild_id)
         member = await guild.fetch_member(payload.user_id)
         role = discord.utils.get(guild.roles, name=ROLE)
         await member.remove_roles(role)
-        
 
-api_key = os.getenv('API_KEY')
+
+api_key = os.getenv("API_KEY")
 bot.run(api_key)
