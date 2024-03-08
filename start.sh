7a91325fe2f9994f1979dd436d908dc13b729284
RomanGoEmpire <roman.gerloff@proton.me>
2024-03-08 15:02:12 +0100

    change start.sh

--- start.sh
+++ start.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
 
-# Update project
-git pull
-
-# Check if the virtual environment exists
-if [ -d "venv" ]
-then
-    echo "Virtual environment exists. Starting..."
-    source venv/bin/activate
-else
-    echo "Virtual environment does not exist. Creating..."
-    python3 -m venv venv
-    source venv/bin/activate
-    # You can add any additional setup steps here if needed
+screen -dmS roles_bot bash -c "
+git pull;
+if [ ! -d 'venv' ]; then
+    python3 -m venv venv;
 fi
-
-# install all requirements
-pip install -r requirements.txt
-
-# start the process
-# Start a screen session named "roles_bot"
-screen -S roles_bot -d -m bash -c 'streamlit run roles_bot.py'
-
-# Close the screen in the background
-screen -S roles_bot -X quit
+source venv/bin/activate;
+pip install -r requirements.txt;
+streamlit run your_app.py;
+bash
+"
